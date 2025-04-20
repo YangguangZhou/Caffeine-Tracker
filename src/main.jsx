d83c2f7b0aa7f5901d41a5f5587b1f2f23d7c6de
Jerry Zhou <i@jerryz.com.cn>
2025-04-20 16:20:53 +0800

    增加预设

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import CaffeineTracker from './App.jsx'
+import CaffeineTracker from './CaffeineTracker.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
