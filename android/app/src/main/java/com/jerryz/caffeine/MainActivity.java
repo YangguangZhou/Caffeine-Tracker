26d7d997de021be12e436b163a3093160bf2e54d
Jerry Zhou <i@jerryz.com.cn>
2025-04-28 21:14:00 +0800

    修复问题

--- android/app/src/main/java/com/jerryz/caffeine/MainActivity.java
+++ android/app/src/main/java/com/jerryz/caffeine/MainActivity.java
@@ -1,5 +1,19 @@
 package com.jerryz.caffeine;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 确保不是全屏模式
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // 设置状态栏颜色
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        //setContentView(R.layout.activity_main);
+    }
+}
